b212aaf61757aece05ec2a076b82a96620931e27
Pratush <pratush.bh@gmail.com>
2022-04-07 00:54:56 +0530

    Validation using express-validator

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
+const { validationResult } = require('express-validator');
 
 const HttpError = require("../models/http-error");
 
@@ -49,6 +50,13 @@ const getPlacesByUserId = (req, res, next) => {
 
 const createPlace = (req, res, next) => {
   //const title = req.body.title
+
+   const errors = validationResult(req);
+   if(!errors.isEmpty()) {
+     //console.log(errors);
+     throw new HttpError('Invalid inputs passed, please check your data', 422);
+   }
+
    const { title, description, coordinates, address, creator } = req.body;
    const createdPlace = {
      id: uuidv4(),
@@ -65,6 +73,12 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    throw new HttpError('Invalid inputs passed, please check your data', 422);
+  }
+
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
@@ -81,6 +95,9 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+  if (!DUMMY_PLACES.find(p => p.id === placeId)) {
+    throw new HttpError('Could not find a place for that id')
+  }
   DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId );
   res.status(200).json({message: 'Deleted place.'});
 };
--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const { check } = require('express-validator');
 
-const usersControllers = require('../controllers/users-controllers')
+const usersController = require('../controllers/users-controllers')
 
 const router = express.Router();
 
-router.get('/', usersControllers.getUsers);
+router.get('/', usersController.getUsers);
 
-router.post('/signup', usersControllers.signup);
+router.post(
+    '/signup', 
+    [
+        check("name")
+            .not()
+            .isEmpty(),
+        check('email').normalizeEmail().isEmail(),
+        check('password').isLength({ min: 6})
 
-router.post('/login', usersControllers.login);
+    ],  
+    usersController.signup
+);    
+
+router.post('/login', usersController.login);
 
 module.exports = router;
