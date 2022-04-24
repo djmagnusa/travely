a5eaa5b7374ccfda7f03eebe324d53477dab9fb8
Pratush <pratush.bh@gmail.com>
2022-04-24 22:55:51 +0530

    Create user from frontend

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/users-routes');
-const HttpError = require('./models/http-error');
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
-require('dotenv').config()
+require("dotenv").config();
 
 const DB = process.env.DATABASE;
 
@@ -14,28 +14,40 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/places', placesRoutes); // /api/places/..
-app.use('/api/users', usersRoutes); // /api/users/..
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); //opening to any domain using '*'
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next(); //allow other middleware to run afterwards
+});
 
-app.use((req, res, next) => { //only runs if no respnose was sent to one of the routes above
-    const error = new HttpError('Could not find this route', 404)
-    throw error;
-}); 
+app.use("/api/places", placesRoutes); // /api/places/..
+app.use("/api/users", usersRoutes); // /api/users/..
 
-app.use((error, req, res, next) => { //
-    if(res.headerSent) {
-        return next(error)
-    }
+app.use((req, res, next) => {
+  //only runs if no respnose was sent to one of the routes above
+  const error = new HttpError("Could not find this route", 404);
+  throw error;
+});
 
-    res.status(error.code || 500);
-    res.json({ message: error.message || 'An unknown error occured!'});
+app.use((error, req, res, next) => {
+  //
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured!" });
 });
 
 mongoose
-    .connect(DB)
-    .then(() => {
-        app.listen(5000);         
-    })
-    .catch(err => {
-        console.log(err);
-    });
+  .connect(DB)
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
